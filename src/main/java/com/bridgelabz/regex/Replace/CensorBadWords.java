0a01777254ec53bd9f2cdfcd6c3383853eb2f285
ankush1038 <ankusharma1029@gmail.com>
2025-04-02 11:10:39 +0530

    [Ankush] Add: Censored words replaced

---
+++ src/main/java/com/bridgelabz/regex/Replace/CensorBadWords.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.regex.Replace;
+
+import java.util.Scanner;
+public class CensorBadWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.println("Enter input with bad words: ");
+        String input = sc.nextLine();
+
+        // Replacing bad words
+        String[] badWords = {"damn", "stupid"};
+
+        for(String word: badWords){
+            input = input.replaceAll("(?i)\\b" + word + "\\b", "*****");
+        }
+
+        //Display censored sentence
+        System.out.println("Censored Output: ");
+        System.out.println(input);
+
+        sc.close();
+    }
+}
---
+++ src/main/java/com/bridgelabz/regex/Replace/ReplaceMultipleSpaces.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.regex.Replace;
+
+import java.util.Scanner;
+
+public class ReplaceMultipleSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.println("Enter a sentence: ");
+        String input = sc.nextLine();
+
+        // Method call
+        replaceSpacesWithSpace(input);
+
+        sc.close();
+    }
+
+    // Method to remove extra spaces
+    public static void replaceSpacesWithSpace(String input){
+        // Output String
+        String output = input.replaceAll("\\s+", " ");
+
+        System.out.println("After removing extra spaces: ");
+        System.out.println(output);
+    }
+}
